7dd98641d4f83fd2531adf39cad2293d43dcb600
Vitor Otero <votero.ext@gmv.com>
2023-05-29 11:01:20 +0100

    Tool NetworkingManager with Combine refactor

--- NetworkingManager.swift
+++ NetworkingManager.swift
@@ -10,7 +10,6 @@ import Combine
 
 class NetworkingManager {
     
-    
 //    private func getCoins() {
 //        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h&locale=en") else {return}
 //
@@ -22,20 +21,35 @@ class NetworkingManager {
 //            })
 //    }
     
+    
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(url: let url): return "[🔥] Bad response from URL. \(url)"
+            case .unknown: return "[⚠️] Unknown error occured"
+            }
+        }
+    }
+    
     static func download(url: URL) -> AnyPublisher<Data, any Error> {
        return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
+            .tryMap({try handleURLResponse(output: $0, url: url) })
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        return output.data
+    }
+        
     static func handleCompletion(completion: Subscribers.Completion<Error>) {
         switch completion {
         case .finished:
@@ -46,4 +60,3 @@ class NetworkingManager {
         }
     }
 }
-
